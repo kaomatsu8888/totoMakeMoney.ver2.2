5d86f13f2af4885edf3ba84759ed257e5874d0fd
Kaoru Matsunaga <m.kaoru0830@gmail.com>
2023-09-14 15:31:53 +0900

    Initial Commit

---
+++ totoMakeMoney.ver2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  totoMakeMoney.ver2.2
+//
+//  Created by kaoru matsunaga on 2023/09/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
